bf025d00bc8ce53788e595b8cd4ec6b6246c41ad
vanilladucky <kimhyunbin106@gmail.com>
2023-03-12 03:54:44 +0900

    preparation for streamlit app

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,20 +1,32 @@
 import boto3
 import json
 import spacy
+import pandas as pd
+from tqdm import tqdm
 
-# Once the sentiment for the conversation reaches a certain negative limit, we can alert social workers who can take human action
+"""
+Once the sentiment for the conversation reaches a certain negative limit, we can alert social workers who can take human action
+"""
 
 comprehend = boto3.client(service_name='comprehend', region_name='ap-southeast-1')
 
-f = open("src/response.txt", "r")
-text = f.read()
+sentiment_df = pd.DataFrame()
+positive_arr = []
+negative_arr = []
 
-print(comprehend.detect_sentiment(Text=text, LanguageCode='en')['Sentiment'])
+for i in tqdm(range(7)):
+    f = open(f"responses/responses_{i+1}.txt", "r")
+    text = f.read()
+    # print(comprehend.detect_sentiment(Text=text, LanguageCode='en')['Sentiment'])
+    positive = comprehend.detect_sentiment(Text=text, LanguageCode='en')['SentimentScore']['Positive']
+    negative = comprehend.detect_sentiment(Text=text, LanguageCode='en')['SentimentScore']['Negative']
+    # print(f"It is {positive*100:.2f}% positive and {negative*100:.2f}% negative")
+    positive_arr.append(positive*100)
+    negative_arr.append(negative*100)
 
-positive = comprehend.detect_sentiment(Text=text, LanguageCode='en')['SentimentScore']['Positive']
-negative = comprehend.detect_sentiment(Text=text, LanguageCode='en')['SentimentScore']['Negative']
-print(f"It is {positive*100:.2f}% positive and {negative*100:.2f}% negative")
-
+sentiment_df['Positive'] = positive_arr
+sentiment_df['Negative']=negative_arr
+print(sentiment_df)
 
 """
 We can also extract some medical data that could be in the conversation
@@ -25,12 +37,16 @@ The model would analyze the conversation line by line and only extract out medic
 """
 
 med7 = spacy.load("en_core_med7_lg")
+data_collected = []
 
-with open('src/response.txt', 'r') as f:
-    last_line = f.readlines()[-1]
+for i in tqdm(range(7)):
+    with open(f"responses/responses_{i+1}.txt", "r") as f:
+        last_line = f.readlines()[-1]
+    text = last_line
+    doc = med7(text)
+    for ent in doc.ents:
+        if len(ent.text) != 0:
+            print(ent.text, ent.label_)
+            data_collected.append((ent.text, ent.label_))
 
-text = last_line
-doc = med7(text)
-print("\n")
-for ent in doc.ents:
-    print(ent.text, ent.label_)
+print(data_collected)
