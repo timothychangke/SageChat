bdc208a76514a65c3ed0f72c0b31888271d0fb9f
vanilladucky <kimhyunbin106@gmail.com>
2023-03-12 14:44:46 +0900

    bug fix

--- src/chatting.js
+++ src/chatting.js
@@ -40,7 +40,7 @@ const Chatting = () => {
         /* Chat-GPT api process.env.REACT_APP_API */
         const { Configuration, OpenAIApi } = require('openai');
         const configuration = new Configuration({
-            apiKey: "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"
+            apiKey: process.env.REACT_APP_API
         });
         const openai = new OpenAIApi(configuration);          
       
